1076891321710ac0947f4a0f4a5939f3fdc8c0dd
Daniel O'Grady <daniel.ogrady@uni-tuebingen.de>
2018-07-10 15:21:54 +0200

    Don't send private fields of players when using get:players anymore

--- Inf3/server/command/client/get/GetUsersCommand.java
+++ Inf3/server/command/client/get/GetUsersCommand.java
@@ -1,13 +1,14 @@
 package command.client.get;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import command.ClientCommand;
 import command.Command;
-import environment.wrapper.ServerPlayer;
+import environment.entity.Player;
 import server.Server;
 import server.TcpClient;
 import util.ServerConst;
@@ -20,13 +21,18 @@ public class GetUsersCommand extends ClientCommand {
 
 	@Override
 	protected int routine(TcpClient src, String cmd, StringBuilder mes) {
-		final List<ServerPlayer> players = new ArrayList<>();
+		Map<String, List<Player>> response = new HashMap<>();
+		// is this threadsafe?
+		/*
 		final Iterator<TcpClient> it = _server.getClients().iterator();
 		while (it.hasNext()) {
 			players.add(it.next().getPlayer());
 		}
-
-		final Optional<String> json = _server.json(players);
+		 */
+		response.put(ServerConst.ANS_USERS, 
+				_server.getClients().stream().map(c -> c.getPlayer().getWrappedObject()).collect(Collectors.toList())
+		);
+		final Optional<String> json = _server.json(response);
 		if (json.isPresent()) {
 			src.send(json.get());
 			mes.append("sent all users to " + src);
